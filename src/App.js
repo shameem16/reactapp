52e9b2d822e52aec9afe226a1a4a2a78a49db791
shameem16 <42208522+shameem16@users.noreply.github.com>
2020-08-27 15:13:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Welcome to netilfy</p> 
+        <p>Welcome to netilfy.Enjoy deploying</p> 
         
         <a
           className="App-link"
